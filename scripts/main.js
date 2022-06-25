58d01b26cea69f0a910609432c9062ac2999c78c
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-06-25 12:07:09 -0300

    Logica Mapa de Contacto

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,8 @@
 const d = document;
 const menuBtn = d.querySelector('nav .btn');
 const menuLst = d.querySelector('nav .menu');
+const mapBtn = d.querySelector('.map .btn');
+const mapFrm = d.querySelector('.map .cover');
 
 // Navegacion
 menuBtn.onclick = () => {
@@ -11,5 +13,11 @@ menuBtn.onclick = () => {
 }
 
 // Mapa de Contacto
-
+mapBtn.addEventListener('click', () => {
+    if (mapFrm.classList.toggle('active')){
+        mapBtn.classList.replace('icon-max','icon-min')
+    } else {
+        mapBtn.classList.replace('icon-min','icon-max')
+    }
+}   )
 // Formulario de contacto
